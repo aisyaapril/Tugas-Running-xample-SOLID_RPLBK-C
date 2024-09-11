0bd49a3bcf70efb51905043a3e8ca449cb79309c
aisyaapril <128948216+aisyaapril@users.noreply.github.com>
2024-09-12 00:14:31 +0700

    Update Test_LSP.py

--- Test_LSP.py
+++ Test_LSP.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 
-# Superclass MediaPlayer
 class MediaPlayer(ABC):
     @abstractmethod
     def playAudio(self):
@@ -10,7 +9,6 @@ class MediaPlayer(ABC):
     def playVideo(self):
         pass
 
-# Subclass dvdPlayer
 class dvdPlayer(MediaPlayer):
     def playAudio(self):
         print("Memutar audio di DVD Player.")
@@ -18,16 +16,13 @@ class dvdPlayer(MediaPlayer):
     def playVideo(self):
         print("Memutar video di DVD Player.")
 
-# Subclass winampPlayer
 class winampPlayer(MediaPlayer):
     def playAudio(self):
         print("Memutar audio di Winamp Player.")
 
-    # Winamp hanya memutar audio, tidak memiliki kemampuan untuk memutar video
     def playVideo(self):
         raise NotImplementedError("Winamp Player tidak mendukung pemutaran video.")
 
-# Fungsi yang menggunakan prinsip Liskov Substitution
 def play_media(player: MediaPlayer):
     player.playAudio()
     try:
@@ -35,10 +30,8 @@ def play_media(player: MediaPlayer):
     except NotImplementedError as e:
         print(e)
 
-# Menggunakan DVD Player
 dvd = dvdPlayer()
 play_media(dvd)
 
-# Menggunakan Winamp Player
 winamp = winampPlayer()
 play_media(winamp)
